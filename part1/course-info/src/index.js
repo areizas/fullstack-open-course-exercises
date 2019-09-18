9ae0f8321f8b6b981d009bbee7b9d33c60ed34dd
areizas <mateo.areiza@quind.io>
2019-09-17 23:48:53 -0500

    Exercise 1.5 - Update Application to generate only one Object called course wich contains all information

--- part1/course-info/src/index.js
+++ part1/course-info/src/index.js
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom'
 const Header = (props) =>{
   return(
       <div>
-          <h1> {props.course}</h1>
+          <h1> {props.course.name}</h1>
       </div>
   )
 };
 
 const Content = (props) =>{
     const contens = [];
-    props.parts.forEach(part => {
+    props.course.parts.forEach(part => {
         contens.push(<p> {part.name} {part.exercises}</p>)
     });
 
@@ -24,39 +24,41 @@ const Content = (props) =>{
 
 const Total = (props) =>{
     let totalEx = 0;
-    props.parts.forEach(part => {
+    props.course.parts.forEach(part => {
         totalEx+=part.exercises;
     });
 
     return(
       <div>
-          <p> {props.message} {totalEx} </p>
+          <p> Total Number of exercises {totalEx} </p>
       </div>
     )
 };
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts=[
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ];
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    };
 
     return (
         <div>
             <Header course={course} />
-            <Content parts={parts}/>
-            <Total message="Number of exercises" parts={parts}/>
+            <Content course={course}/>
+            <Total course={course}/>
         </div>
     )
 };
