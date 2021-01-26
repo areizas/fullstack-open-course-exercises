5a3c52c7dc0accbfc62dcc4ac3de6108fa467157
areizas <areizas96@gmail.com>
2021-01-26 16:13:16 -0500

    set CHOKIDAR_USEPOLLING=true on the npm start command to reload the code changes

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => (
-    <div>
-        <p>Hello world 2</p>
-    </div>
-)
+const App = () => {
+
+    console.log('Hello from App component')
+
+    return (
+        <div>
+            <p>Hello world</p>
+        </div>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
