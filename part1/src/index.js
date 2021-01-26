0ccbb957049e388c3eaf81b793f76a65e8b7c962
areizas <areizas96@gmail.com>
2021-01-26 16:20:52 -0500

    Using functions and variables inside App component

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-
     console.log('Hello from App component')
+
+    const now = new Date()
+    const a = 10
+    const b = 15
 
     return (
         <div>
-            <p>Hello world</p>
+            <p>Hello world, it is {now.toString()}</p>
+            <p> {a} + {b} = {a+b} </p>
         </div>
     )
 }
