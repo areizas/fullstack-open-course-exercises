f71cd6430f1831fab4c83d5f406201af9aa68f24
areizas <areizas96@gmail.com>
2021-01-28 22:29:24 -0500

    Part 1d: Create 2 different states

--- part1/1-notes/src/index.js
+++ part1/1-notes/src/index.js
@@ -2,23 +2,17 @@ import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
 
-const Display = ({counter}) => <div>{counter}</div>
-
-const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
-
 const App = () => {
-    const [counter, setCounter] = useState(0)
 
-    const increaseByOne = () => setCounter(counter+1)
-    const decreaseByOne = () => setCounter(counter-1)
-    const setToZero = () => setCounter(0)
+    const [right, setRight] = useState(0)
+    const [left, setLeft] = useState(0)
 
     return (
         <>
-            <Display counter={counter}/>
-            <Button handleClick={decreaseByOne} text={"-"} />
-            <Button handleClick={setToZero} text={"0"}/>
-            <Button handleClick={increaseByOne} text={"+"}/>
+            <div>{left}</div>
+            <button onClick={() => setLeft(left+1) }>left</button>
+            <div>{right}</div>
+            <button onClick={() => setRight(right+1) }>right</button>
         </>
     )
 }
