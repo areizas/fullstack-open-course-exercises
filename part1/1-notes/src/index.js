fdfc7b2590987430b145ad23d942811fca0b6a86
areizas <areizas96@gmail.com>
2021-01-28 22:14:16 -0500

    Part 1c: Create Button component

--- part1/1-notes/src/index.js
+++ part1/1-notes/src/index.js
@@ -1,17 +1,36 @@
 import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
+
+const Display = (props) => {
+    return (
+        <>
+            <p>{props.counter}</p>
+        </>
+    )
+}
+
+const Button = (props) => {
+    return(
+        <>
+            <button onClick={props.handleClick}>{props.text}</button>
+        </>
+    )
+}
+
 const App = () => {
     const [counter, setCounter] = useState(0)
 
-    const handleButton = () => setCounter(counter+1)
+    const increaseByOne = () => setCounter(counter+1)
+    const decreaseByOne = () => setCounter(counter-1)
+    const setToZero = () => setCounter(0)
 
     return (
         <>
-            <p>{counter}</p>
-            <button onClick={() => setCounter(counter-1)}>-</button>
-            <button onClick={() => setCounter(0)}>zero</button>
-            <button onClick={() => setCounter(counter+1)}>+</button>
+            <Display counter={counter}></Display>
+            <Button handleClick={decreaseByOne} text={"-"}></Button>
+            <Button handleClick={setToZero} text={"0"}></Button>
+            <Button handleClick={increaseByOne} text={"+"}></Button>
         </>
     )
 }
