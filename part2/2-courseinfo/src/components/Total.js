3b61bc455ab319b70ba00d98a805ce2867d34173
areizas <areizas96@gmail.com>
2021-02-01 17:44:19 -0500

    Ex 2.2: Refactor course info app to use self file components, the Course component has been created

---
+++ part2/2-courseinfo/src/components/Total.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Total = ({parts}) => {
+    return (
+        <>
+            <p>Number of exercises {parts.reduce((a,b) => a + b.exercises, 0)}</p>
+        </>
+    )
+}
+
+export default Total
