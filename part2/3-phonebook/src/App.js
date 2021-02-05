45c31b53982a993c7c0f7504eb363c69a96878c0
areizas <areizas96@gmail.com>
2021-02-05 14:09:12 -0500

    Ex 2.11 - Create json-server with initial data, in addition, the axios library is being used to fletch the data from server

--- part2/3-phonebook/src/App.js
+++ part2/3-phonebook/src/App.js
@@ -1,14 +1,7 @@
 import React from "react";
 import PhoneBook from "./components/Phonebook";
 
-const App = () => {
-
-    const initialPersons = [
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ]
+const App = ({initialPersons}) => {
 
     return (
         <>
--- part2/3-phonebook/src/index.js
+++ part2/3-phonebook/src/index.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from "./App";
+import axios from "axios";
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+const initialPersons = [
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+]
+axios
+    .get("http://localhost:3001/persons")
+    .then( response => {
+        const persons = response.data
+        ReactDOM.render(<App initialPersons={persons}/>, document.getElementById('root'))
+    })
+
