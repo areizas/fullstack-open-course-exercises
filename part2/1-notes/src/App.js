61f8bc427c201436ac971d2ef8bd1f6e64099b59
areizas <areizas96@gmail.com>
2021-02-06 16:39:35 -0500

    Part 2d: Update notes App to use css styling

--- part2/1-notes/src/App.js
+++ part2/1-notes/src/App.js
@@ -1,11 +1,27 @@
 import React, {useState, useEffect} from 'react'
 import Note from "./components/Note";
 import noteService from './services/notes'
+import Notification from "./components/Notification";
+
+const Footer = () => {
+    const footerStyle = {
+        color: 'green',
+        fontStyle: 'italic',
+        fontSize: 16
+    }
+    return (
+        <div style={footerStyle}>
+            <br />
+            <em>Note app, Mateo is learning React thanks to: Department of Computer Science, University of Helsinki</em>
+        </div>
+    )
+}
 
 const App = () => {
     const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState('a new note...')
     const [showAll, setShowAll] = useState(true)
+    const [errorMessage, setErrorMessage] = useState('some error happened...')
 
     useEffect(()=>{
         noteService
@@ -44,9 +60,10 @@ const App = () => {
                 setNotes(notes.map( note => note.id!==id ? note : returnedNote))
             })
             .catch(error => {
-                alert(
-                    `the note '${note.content}' was already deleted from server`
-                )
+                setErrorMessage(`Note '${note.content}' was already removed from server`)
+                setTimeout(()=>{
+                    setErrorMessage(null)
+                },5000)
                 setNotes(notes.filter(n => n.id !== id))
             })
     }
@@ -56,6 +73,7 @@ const App = () => {
     return (
         <div>
             <h1>Notes</h1>
+            <Notification message={errorMessage}/>
             <button onClick={()=>setShowAll(!showAll)}>
                 show {showAll ? 'important' : 'all'}
             </button>
@@ -71,6 +89,7 @@ const App = () => {
                 />
                 <button type="submit">save</button>
             </form>
+            <Footer/>
         </div>
     )
 }
