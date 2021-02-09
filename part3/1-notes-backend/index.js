a11c1d69cec4fd31c61527fc285ccd37bd9be62b
areizas <areizas96@gmail.com>
2021-02-09 18:45:13 -0500

    Part 3c - Update Note application to use heroku server

--- part3/1-notes-backend/index.js
+++ part3/1-notes-backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -8,7 +9,8 @@ const requestLogger = (request, response, next) => {
     console.log('---')
     next()
 }
-
+app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
@@ -32,10 +34,6 @@ let notes = [
         important: true
     }
 ]
-
-app.get('/', (request,response) => {
-    response.send('<h1>Hello World</h1>')
-})
 
 app.get('/api/notes', (request,response)=>{
     response.json(notes)
@@ -91,7 +89,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
