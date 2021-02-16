c695a338933f2cee08ad9d35122a71836e8baa46
areizas <areizas96@gmail.com>
2021-02-16 16:53:56 -0500

    Ex 4.2 - Create models directory

--- part4/5-blog/app.js
+++ part4/5-blog/app.js
@@ -1,19 +1,9 @@
 require('dotenv').config()
-const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const logger = require('./utils/logger')
-
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 const mongoUrl = process.env.MONGOOSE_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
