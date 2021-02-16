98d4a7db1a6752a8776499a387cc653b7140f8fb
areizas <areizas96@gmail.com>
2021-02-16 17:22:22 -0500

    Ex 4.2 - Create middleware util

--- part4/5-blog/app.js
+++ part4/5-blog/app.js
@@ -5,12 +5,18 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const Blog = require('./models/blog')
 const blogRouter = require('./controllers/blogs')
+const morgan = require('morgan')
+const middleware = require('./utils/middleware')
 
 mongoose.connect(config.MONGOOSE_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
 app.use(express.json())
+app.use(morgan('tiny'))
 
 app.use('/api/blogs',blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
---
+++ part4/5-blog/utils/middleware.js
@@ -0,0 +1,20 @@
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).json({error: 'unknown endpoint'})
+}
+
+const errorHandler = (error, request, response, next) => {
+
+    if( error.name === 'ValidationError' ){
+        response.status(400).json({ error: error.message })
+    } else if ( error.name === 'CastError' ){
+        response.status(400).json({ error: 'malformed id' })
+    }
+
+    next(error)
+
+}
+
+module.exports = {
+    unknownEndpoint, errorHandler
+}
