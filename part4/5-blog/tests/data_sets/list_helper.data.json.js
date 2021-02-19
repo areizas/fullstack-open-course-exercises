12041fea25784fea393d301ef950f7e224991ebb
areizas <areizas96@gmail.com>
2021-02-19 17:12:55 -0500

    Part 4a - Create data set to store test sample data for list helper

---
+++ part4/5-blog/tests/data_sets/list_helper.data.json.js
@@ -0,0 +1,39 @@
+const listWithOneBlog = [
+    {
+        id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5
+    }
+]
+
+const listWithMultipleBlogs = [
+    {
+        id: '1',
+        title: 'title 1',
+        author: 'author 1',
+        url: 'http://url1.com',
+        likes: 1
+    },
+    {
+        id: '2',
+        title: 'title 2',
+        author: 'author 2',
+        url: 'http://url2.com',
+        likes: 2
+    },
+    {
+        id: '3',
+        title: 'title 3',
+        author: 'author 3',
+        url: 'http://url3.com',
+        likes: 3
+    },
+]
+
+const emptyList = []
+
+module.exports = {
+    listWithOneBlog, listWithMultipleBlogs, emptyList
+}
