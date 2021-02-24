896275b0a18f49d67fbf203cfcf56683a3e7209b
areizas <areizas96@gmail.com>
2021-02-23 19:49:45 -0500

    Part 4b - Update files according Part 4 guide

--- part3/1-notes-backend/utils/config.js
+++ part3/1-notes-backend/utils/config.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGO_URI = process.env.MONGO_URI
+let MONGO_URI = process.env.MONGO_URI
+
+if (process.env.NODE_ENV === 'test'){
+    MONGO_URI = process.env.TEST_MONGODB_URI
+}
 
 module.exports = {
     MONGO_URI,
